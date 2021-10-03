c2e73f48073a30d4366f34aad2e8f654d9310cf1
Chris Mumford <cmumford@cmumford.com>
2021-10-03 15:54:19 -0700

    Printing out memory info at startup.
    ```
    I (317) App: CapTouchProto app!
    I (317) App: This is esp32 chip with 2 CPU core(s), WiFi/BT/BLE.
    I (327) App: Silicon revision 3.
    I (327) App: 4 MB external flash
    I (337) App: Free heap size: 292772 bytes.
    I (337) App: Free internal heap size: 292772 bytes.
    I (347) App: Minimum free heap size: 291600 bytes.
    I (347) App: MALLOC_CAP_EXEC: 214720 bytes.
    I (357) App: MALLOC_CAP_32BIT: 404888 bytes.
    I (357) App: MALLOC_CAP_8BIT: 319080 bytes.
    I (367) App: MALLOC_CAP_8BIT: 319080 bytes.
    I (367) App: MALLOC_CAP_DMA: 319080 bytes.
    I (377) App: MALLOC_CAP_PID2: 0 bytes.
    I (377) App: MALLOC_CAP_PID3: 0 bytes.
    I (387) App: MALLOC_CAP_PID4: 0 bytes.
    I (387) App: MALLOC_CAP_PID5: 0 bytes.
    I (397) App: MALLOC_CAP_PID6: 0 bytes.
    I (397) App: MALLOC_CAP_PID7: 0 bytes.
    I (397) App: MALLOC_CAP_SPIRAM: 0 bytes.
    I (407) App: MALLOC_CAP_INTERNAL: 404888 bytes.
    I (407) App: MALLOC_CAP_DEFAULT: 319080 bytes.
    I (417) App: MALLOC_CAP_IRAM_8BIT: 0 bytes.
    ```

--- main/main.cc
+++ main/main.cc
@@ -24,8 +24,42 @@ void LogHardwareInfo() {
       TAG, "%zu MB %s flash", spi_flash_get_chip_size() / (1024 * 1024),
       (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
+  ESP_LOGI(TAG, "Free heap size: %u bytes.", esp_get_free_heap_size());
+  ESP_LOGI(TAG, "Free internal heap size: %u bytes.",
+           esp_get_free_internal_heap_size());
   ESP_LOGI(TAG, "Minimum free heap size: %u bytes.",
            esp_get_minimum_free_heap_size());
+
+  ESP_LOGI(TAG, "MALLOC_CAP_EXEC: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_EXEC));
+  ESP_LOGI(TAG, "MALLOC_CAP_32BIT: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_32BIT));
+  ESP_LOGI(TAG, "MALLOC_CAP_8BIT: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_8BIT));
+  ESP_LOGI(TAG, "MALLOC_CAP_8BIT: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_8BIT));
+  ESP_LOGI(TAG, "MALLOC_CAP_DMA: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_DMA));
+  ESP_LOGI(TAG, "MALLOC_CAP_PID2: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_PID2));
+  ESP_LOGI(TAG, "MALLOC_CAP_PID3: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_PID3));
+  ESP_LOGI(TAG, "MALLOC_CAP_PID4: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_PID4));
+  ESP_LOGI(TAG, "MALLOC_CAP_PID5: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_PID5));
+  ESP_LOGI(TAG, "MALLOC_CAP_PID6: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_PID6));
+  ESP_LOGI(TAG, "MALLOC_CAP_PID7: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_PID7));
+  ESP_LOGI(TAG, "MALLOC_CAP_SPIRAM: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_SPIRAM));
+  ESP_LOGI(TAG, "MALLOC_CAP_INTERNAL: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_INTERNAL));
+  ESP_LOGI(TAG, "MALLOC_CAP_DEFAULT: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_DEFAULT));
+  ESP_LOGI(TAG, "MALLOC_CAP_IRAM_8BIT: %u bytes.",
+           heap_caps_get_total_size(MALLOC_CAP_IRAM_8BIT));
 }
 
 }  // namespace
