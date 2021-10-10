2c110a09cee4f0b9b7c4a18f054acd2cf02f51b7
Chris Mumford <cmumford@cmumford.com>
2021-10-10 08:13:36 -0700

    Added script to delete files.
    Simplify the cleaning process with a script to delete files/dirs.
    Adding this because PowerShell's Remove-Item command doesn't take
    multiple args, and also returns a non-zero error code for nonexistent
    items - even with the `-ErrorAction Ignore` argument.

---
+++ scripts/delete-items.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# A helper script to delete filesystem objects (AKA files & directories).
+# All directories will be deleted recursively.
+
+import argparse
+from shutil import rmtree
+import os
+
+# 0 => quiet, 1 => normal, 2 => verbose.
+verbosity = 1
+noop = False
+
+
+class InvalidItemType(Exception):
+    pass
+
+
+def DeleteItem(item):
+    global noop, verbosity
+    if verbosity > 1:
+        print('Deleting \"%s\"' % item)
+    if noop:
+        return
+    if not os.path.exists(item):
+        if verbosity > 0:
+            print('does not exist: "%s"' % item)
+        return
+    if os.path.isdir(item):
+        rmtree(item)
+    elif os.path.isfile(item):
+        os.remove(item)
+    else:
+        raise InvalidItemType('"%s" is not a file or dir' % item)
+
+
+def DeleteItems(items):
+    for item in items:
+        DeleteItem(item)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Delete filesystem objects.')
+    parser.add_argument('--noop', action='store_true',
+                        help="Print items to delete - don't actually delete.")
+    parser.add_argument('item', nargs='+',
+                        help='Filesystem item to delete')
+
+    args = parser.parse_args()
+    if args.noop:
+        noop = args.noop
+
+    DeleteItems(args.item)
