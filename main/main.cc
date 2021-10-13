20499ac534be4fe139ac2c5d9aca3d9dac49f52f
Chris Mumford <cmumford@cmumford.com>
2021-10-13 07:20:08 -0700

    Rename lv_examples to lv_demos.
    lv_examples seems to have been renamed upstream to lv_demos. Navigating
    to https://github.com/lvgl/lv_examples.git redirects to
    https://github.com/lvgl/lv_demos. This projects lv_examples directory
    started out as a copy of
    https://github.com/lvgl/lv_port_esp32/tree/master/components/lv_examples
    which appears to be before the rename.

--- main/main.cc
+++ main/main.cc
@@ -17,13 +17,13 @@
 
 #ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
 #if defined CONFIG_LV_USE_DEMO_WIDGETS
-#include <lv_examples/src/lv_demo_widgets/lv_demo_widgets.h>
+#include <lv_demos/src/lv_demo_widgets/lv_demo_widgets.h>
 #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
-#include <lv_examples/src/lv_demo_keypad_encoder/lv_demo_keypad_encoder.h>
+#include <lv_demos/src/lv_demo_keypad_encoder/lv_demo_keypad_encoder.h>
 #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
-#include <lv_examples/src/lv_demo_benchmark/lv_demo_benchmark.h>
+#include <lv_demos/src/lv_demo_benchmark/lv_demo_benchmark.h>
 #elif defined CONFIG_LV_USE_DEMO_STRESS
-#include <lv_examples/src/lv_demo_stress/lv_demo_stress.h>
+#include <lv_demos/src/lv_demo_stress/lv_demo_stress.h>
 #else
 #error "No demo application selected."
 #endif
